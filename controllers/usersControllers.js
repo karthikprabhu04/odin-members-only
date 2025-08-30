b448936368cbbfd512c2007b753f164da1feeab6
karthikprabhu04 <kpp32@cam.ac.uk>
2025-08-30 21:55:22 +0100

    Create sign in form
    Sanitzied and validated form fields, including confirmPassword.

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,13 +1,70 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage("First Name must only include letters.")
+    .isLength({ min: 1, max: 30 })
+    .withMessage("First Name must be between 1 and 30 characters"),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage("Last Name must only include letters.")
+    .isLength({ min: 1, max: 30 })
+    .withMessage("Last Name must be between 1 and 30 characters"),
+  body("username")
+    .trim()
+    .isLength({ min: 1, max: 30 })
+    .withMessage("Username must be between 1 and 30 characters")
+    .custom(async (username) => {
+      // Check if username is taken
+      await db.checkUsernameAvailability(username);
+    }),
+  body("password")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("Password should have at least 3 characters"),
+  body("confirmPassword")
+    .trim()
+    .custom((confirmPassword, { req }) => {
+      if (confirmPassword !== req.body.password) {
+        throw new Error("Passwords do not match")
+      }
+      return true;
+    }),
+];
 
 exports.index = async (req, res) => {
-  res.render("index");
+  res.render("index", {
+    firstName: "",
+    lastName: "",
+    username: "",
+    password: "",
+    confirmPassword: ""
+  });
 };
 
-exports.signup = async (req, res) => {
-  console.log("Adding user...");
-  const { firstName, lastName, username, password } = req.body;
-  await db.addUser(firstName, lastName, username, password);
-  console.log("User added!");
-  res.redirect("/");
-};
+exports.signup = [
+  validateUser,
+  async (req, res) => {
+    const errors = validationResult(req);
+    const { firstName, lastName, username, password, confirmPassword } = req.body;
+    if (!errors.isEmpty()) {
+      return res.status(400).render("index", {
+        errors: errors.array(),
+        firstName,
+        lastName,
+        username,
+        password,
+        confirmPassword
+      })
+    }
+
+    console.log("Adding user...");
+    await db.addUser(firstName, lastName, username, password);
+    console.log("User added!");
+    res.redirect("/");
+  },
+];
--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,25 @@
 const pool = require("./pool");
 
 async function addUser(firstName, lastName, username, password) {
-  await pool.query(`
-    INSERT INTO users (first_name, last_name, username, password) VALUES
-    ($1, $2, $3, $4);
-  `, [firstName, lastName, username, password]);
+  await pool.query(
+    `INSERT INTO users (first_name, last_name, username, password) VALUES
+    ($1, $2, $3, $4);`,
+    [firstName, lastName, username, password]
+  );
+}
+
+async function checkUsernameAvailability(username) {
+  const result = await pool.query(
+    `SELECT * FROM users WHERE username = $1`,
+    [username]
+  );
+  if (result.rows.length > 0) {
+    throw new Error("Username already taken");
+  }
+  return true;
 }
 
 module.exports = {
   addUser,
-}
+  checkUsernameAvailability,
+};
